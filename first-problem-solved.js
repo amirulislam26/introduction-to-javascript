86d693e09ea061976a1484a3bb8601591eec62be
amirulislam26 <26techamirulislam@gmail.com>
2022-09-20 09:01:34 -0700

    first-problem-solved

---
+++ first-problem-solved.js
@@ -0,0 +1,7 @@
+var totalAmount = 1000;
+var mangoPrice = 200;
+var applePrice = 300;
+
+var totalCost = mangoPrice + applePrice;
+var newTotalAmount = totalAmount - totalCost;
+console.log(newTotalAmount);
