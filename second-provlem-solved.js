1c19527dc9ccc7c12606277e2f3de31fb01bc9f7
amirulislam26 <26techamirulislam@gmail.com>
2022-09-20 09:07:25 -0700

    second-provlem-solved

---
+++ second-provlem-solved.js
@@ -0,0 +1,12 @@
+var math = 75.25;
+var bio = 65;
+var chemistry = 80;
+var physics = 35.45;
+var English = 99.50;
+
+
+var totalMark = math + bio + chemistry + physics + English;
+var AverageMark = totalMark / 5;
+let total = AverageMark;
+let noWMarks = total.toFixed(2);
+console.log(noWMarks);
